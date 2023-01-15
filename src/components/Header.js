97e64ee9bec06b3b7886d3787ec02c121ad4be11
Ken Sarowiwa <ken.wandolah@yahoo.com>
2023-01-15 23:28:11 +0300

    Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      <!-- Navbar -->
+      
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <!-- Container wrapper -->
+  
   <div class="container-fluid">
-    <!-- Toggle button -->
+    
     <button
       class="navbar-toggler"
       type="button"
@@ -20,9 +20,9 @@ function Header() {
       <i class="fas fa-bars"></i>
     </button>
 
-    <!-- Collapsible wrapper -->
+   
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- Navbar brand -->
+      
       <a class="navbar-brand mt-2 mt-lg-0" href="#">
         <img
           src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
@@ -31,7 +31,7 @@ function Header() {
           loading="lazy"
         />
       </a>
-      <!-- Left links -->
+      
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="#">Dashboard</a>
@@ -43,18 +43,17 @@ function Header() {
           <a class="nav-link" href="#">Projects</a>
         </li>
       </ul>
-      <!-- Left links -->
+      
     </div>
-    <!-- Collapsible wrapper -->
 
-    <!-- Right elements -->
+    
     <div class="d-flex align-items-center">
-      <!-- Icon -->
+     
       <a class="text-reset me-3" href="#">
         <i class="fas fa-shopping-cart"></i>
       </a>
 
-      <!-- Notifications -->
+      
       <div class="dropdown">
         <a
           class="text-reset me-3 dropdown-toggle hidden-arrow"
@@ -82,7 +81,7 @@ function Header() {
           </li>
         </ul>
       </div>
-      <!-- Avatar -->
+      
       <div class="dropdown">
         <a
           class="dropdown-toggle d-flex align-items-center hidden-arrow"
@@ -116,11 +115,11 @@ function Header() {
         </ul>
       </div>
     </div>
-    <!-- Right elements -->
+    
   </div>
-  <!-- Container wrapper -->
+  
 </nav>
-<!-- Navbar -->
+
     </div>
   )
 }
