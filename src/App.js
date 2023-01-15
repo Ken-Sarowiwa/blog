e2132e88b8ad02fca0c8bd3778a6af5ba4d97d59
Ken Sarowiwa <ken.wandolah@yahoo.com>
2023-01-15 23:27:58 +0300

    Header navbar

--- src/App.js
+++ src/App.js
@@ -8,18 +8,24 @@ import Home from './pages/Home';
 import { About } from './pages/About';
 import {ToastContainer} from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-          <Route path= "/" element={<Home />}/>
-          <Route path= "/about" element={<About />}/>
-          <Route path= "/addblog" element={<AddEdit />}/>
-          <Route path= "/editBlog/ :id" element={<AddEdit />}/>
-          <Route path= "/blog :id" element={<Blog />}/>
-          <Route path= "*" element={<Notfound />}/>
-      </Routes>
+    <div className='App'>
+      <Header/>
+      <ToastContainer/>
+        <Routes>
+            <Route path= "/" element={<Home />}/>
+            <Route path= "/about" element={<About />}/>
+            <Route path= "/addblog" element={<AddEdit />}/>
+            <Route path= "/editBlog/ :id" element={<AddEdit />}/>
+            <Route path= "/blog :id" element={<Blog />}/>
+            <Route path= "*" element={<Notfound />}/>
+        </Routes>
+      </div>
     </BrowserRouter>
+   
   );
 }
 
