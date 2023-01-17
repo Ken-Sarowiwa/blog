c78bb47980f957e44ea3c255fc7e027957f8b230
Ken Sarowiwa <ken.wandolah@yahoo.com>
2023-01-17 16:31:27 +0300

    header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,58 +2,56 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-      
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  
-  <div class="container-fluid">
-    
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-mdb-toggle="collapse"
-      data-mdb-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
+    <div className='mainheader'>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i class="fas fa-bars"></i>
+          </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-   
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      
       <a class="navbar-brand mt-2 mt-lg-0" href="#">
         <img
-          src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+          src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.https://www.123rf.com/photo_155572084_guitar-vector-bass-ukulele-icon-logo-symbol-music-cartoon-character-graphic-illustration-doodle-desi.html?vti=lm6lkihsyx5qejr1zd-1-2"
           height="15"
-          alt="MDB Logo"
+          alt=""
           loading="lazy"
         />
       </a>
       
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">Dashboard</a>
+          <a class="nav-link" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Team</a>
+          <a class="nav-link" href="/about">About us</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Projects</a>
+          <a class="nav-link" href="/blog :id">Blogs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/editBlog/ :id">Add New</a>
         </li>
       </ul>
       
-    </div>
+      </div>
 
     
     <div class="d-flex align-items-center">
      
-      <a class="text-reset me-3" href="#">
+      <a class="text-reset me-3" href="https://ken-sarowiwa.github.io/KenafricaGram/">
         <i class="fas fa-shopping-cart"></i>
       </a>
 
-      
+
       <div class="dropdown">
         <a
           class="text-reset me-3 dropdown-toggle hidden-arrow"
@@ -61,7 +59,7 @@ function Header() {
           id="navbarDropdownMenuLink"
           role="button"
           data-mdb-toggle="dropdown"
-          aria-expanded="false"
+          aria-expanded="true"
         >
           <i class="fas fa-bell"></i>
           <span class="badge rounded-pill badge-notification bg-danger">1</span>
