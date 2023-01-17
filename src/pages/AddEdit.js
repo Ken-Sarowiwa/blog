fa0d38c60917f8b49d15803ddeb689447f0eef0f
Ken Sarowiwa <ken.wandolah@yahoo.com>
2023-01-17 16:31:35 +0300

    add edit page

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import {useState} from "react"
+import {MDBValidation, MDBInput, MDBBtn} from "mdb-react-ui-kit"
+import axios from 'axios'
+import { Toast } from 'react-toastify'
+//za5gjpw7
 
+
+const initialstate = {
+  title : "",
+  description: "",
+  category : "",
+  imageUrl : "",
+}
+
+const blog_options = ["Travel", "Fashio", "Fitenss", "Sports", "Food", "Tech"];
 function AddEdit() {
+  const [formData, setFormData] = useState(initialstate);
+  const [error, setError]  = useState(null);
+  const {title, description,category,imageUrl} = formData;
+
+  const handleSubmit = () => {
+
+  };
+  
   return (
-    <div><h2>AddEdit</h2></div>
+    <MDBValidation className='row g-3' style={{marginTop: "100px"}} noValidate onSubmit={handleSubmit}>
+      <p className='fs-2 fw-bold'>Add Blog</p>
+    </MDBValidation>
   )
 }
 
