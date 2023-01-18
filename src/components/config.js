9508dc009bcd68ef57baa0973982146ec36b86a7
Ken Sarowiwa <ken.wandolah@yahoo.com>
2023-01-18 22:09:31 +0300

    config

---
+++ src/components/config.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function config() {
+
+const config = {
+    API_KEY: "187512453143324",
+    API_SECRET: "HBh2d6-CNHtN8dIapbG5g5crESk"
+}
+  return (
+    <div>config</div>
+  )
+}
+
+export default config
+
