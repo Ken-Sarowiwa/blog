15c19ab9d27efea80d450cf3d15b75f7b9101fa8
Ken Sarowiwa <ken.wandolah@yahoo.com>
2023-02-19 22:55:36 +0300

    configure the axios params and post request

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -29,14 +29,14 @@ function AddEdit() {
     if (title && description&& category&& imageUrl){
       const updated = ({...formData});
       console.log(updated);
-      const response = await axios.post("http://localhost:3001/blogs", updated);
+      const response =  await axios.post("http://localhost:3000/blogs", updated);
       if (response.status === 201){
         toast.info("Blog updated successfully")
       }else{
         toast.error("upload failed");
       }
-    setFormData({title:"", description: "", category: "", imageUrl: "",})
-    Navigate("/")
+    setFormData({title:"", description: "", category: "", imageUrl: "",});
+    Navigate("/");
     }
   };
 
@@ -120,7 +120,7 @@ function AddEdit() {
       </select>
       <br></br>
       <br></br>
-      <MDBBtn type='submit' style={{marginRight: "10px"}}>Add</MDBBtn>
+      <MDBBtn type='submit' style={{marginRight: "10px"}} onClick={handleSubmit}>Add</MDBBtn>
       <MDBBtn type='Danger' style={{marginRight: "10px"}} onClick={()=>Navigate("/")}>Go back</MDBBtn>
       
       </div>
